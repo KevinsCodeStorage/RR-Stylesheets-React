792bb7c15e3f9a95180b9ba2a7bfaafc85b5ed77
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-02-07 18:33:29 -0500

    created very basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <AboutUs/>
+      <ContactUs/>
+      <Profile/>
     </div>
   );
 }
